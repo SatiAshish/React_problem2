e83a6ab1b886236e0e2d1fe75a2f77352a0ec5a3
Ashish sati <ashishsati27082002@gmail.com>
2022-11-11 11:07:06 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import IMG1 from './image/img1.jpg';
+import IMG2 from './image/img2.jpg';
+import IMG3 from './image/img3.jpg';
+import IMG4 from './image/img4.jpg';
+import IMG5 from './image/img5.jpg';
+import IMG6 from './image/img6.jpg';
+
+const list = [
+    {
+      id: 1,
+      title: "boAt Rockerz 550 Over Ear Bluetooth Headphones(Black)",
+      price: 1499,
+      img: IMG1,
+      amount: 1,
+    },
+    {
+      id: 2,
+      title: "boAt Stone 180 5W Bluetooth Speaker(Black)",
+      price: 2499,
+      img: IMG2,
+      amount: 1,
+    },
+    {
+      id: 3,
+      title: "Pro Professional Collar Mic",
+      price: 999,
+      img: IMG3,
+      amount: 1,
+    },
+    {
+      id: 4,
+      title: "Watch",
+      price: 1299,
+      img: IMG4,
+      amount: 1,
+    },
+    {
+      id: 5,
+      title: "Leptop",
+      price: 39999,
+      img: IMG5,
+      amount: 1,
+    },
+    {
+      id: 6,
+      title: "Cemera",
+      price: 34999,
+      img: IMG6,
+      amount: 1,
+    }
+]
+
+export default list;
